9db798829d0a766015b56aa981969fae90a9dfbc
Luka Arsov <arsovluka@gmail.com>
2025-10-07 00:01:04 +0200

    Izmenjen EventControler.php i dodata kolona descrition u events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -17,17 +17,28 @@ class EventController extends Controller
     // Kreiranje novog događaja
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string',
-            'date' => 'required|date',
-            'location' => 'required|string',
-            'price' => 'required|numeric'
-        ]);
+       try {
+           $request->validate([
+               'name' => 'required|string',
+               'date' => 'required|date',
+               'location' => 'required|string',
+               'price' => 'required|numeric',
+               'description' => 'nullable|string',
+            ]);
 
-        $event = Event::create($request->all());
+            $event = Event::create($request->all());
 
-        return response()->json($event, 201);
+            return response()->json($event, 201);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Greška prilikom kreiranja eventa',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
+
+
 
     // Dohvatanje jednog događaja
     public function show($id)
